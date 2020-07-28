7f9f043184707c67f982b149d840f5a32a234bab
pedro.yoda@gmail.com <Pedro Yoda Saito>
2020-07-28 20:43:59 -0300

    Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import Menu from './components/Menu'
+import Footer from './components/Footer';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import dados from './data/dados_iniciais.json';
 
 function App() {
+
+  const categorias = () => {
+    return dados.categorias.map(categoria => {
+      return (
+        <Carousel ignoreFirstVideo={false} category={categoria}></Carousel>
+      );
+    });
+  }
+
   return (
     <div>
       <Menu></Menu>
+      <BannerMain
+        videoTitle="Free Fire"
+        videoDescription="LevelUp 007 - FIZ UMA JOGADA IMPRESSIONANTE! CONTRA OS MELHORES TIMES FREE FIRE"
+        url="https://www.youtube.com/watch?v=mpS6UhUPjPY"
+      ></BannerMain>
+      {categorias()}
+      <Footer></Footer>
     </div>
   );
 }
